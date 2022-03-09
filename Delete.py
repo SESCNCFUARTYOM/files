09a9368c2008b580bfe1509ef97985dc70202021
SESCNCFUARTYOM <perepelukovartem@gmail.com>
2022-03-09 22:47:27 +0300

    Добавлены

---
+++ Delete.py
@@ -0,0 +1,12 @@
+path = input()
+with open(path, "r", encoding="utf-8") as f:
+    a = f.readlines()
+
+with open("file.py", "w", encoding="utf-8") as f:
+    for i in a:
+        for j in i:
+            if j == "#":
+                i = i[:i.find(j)]
+        f.write(i)
+
+
